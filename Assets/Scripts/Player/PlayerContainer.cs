955de03d535e8e402e32eb9c5f1172d4b268c4b4
Sina Bolouki <sina.bolouki@gmail.com>
2025-05-27 12:59:19 +0200

    add PlayerController

---
+++ Assets/Scripts/Player/PlayerContainer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerContainer : MonoBehaviour
+    {
+        
+        #region Singleton
+        private static PlayerContainer _instance;
+
+        public static PlayerContainer Instance
+        {
+            get => _instance;
+            set => _instance = value;
+        }
+
+        private void Awake()
+        {
+            if (!_instance)
+            {
+                _instance = this;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private InputActionAsset playerActionAsset;
+        [SerializeField] private float moveSpeed;
+
+        private InputAction _moveAction;
+        private Rigidbody2D _rigidbody2D;
+
+
+        private void Awake()
+        {
+            var actionMap = playerActionAsset.FindActionMap("Player", true);
+            _moveAction = actionMap.FindAction("Move", true);
+            _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            Vector2 moveInput = _moveAction.ReadValue<Vector2>();
+            moveInput *= moveSpeed;
+            _rigidbody2D.linearVelocity = moveInput;
+        }
+    }
+}
