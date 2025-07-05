b3886eccbda51d8b4b72b001c5dc0eea1def1c0a
Sina Bolouki <sina.bolouki@gmail.com>
2025-07-05 17:27:08 +0200

    add moving sounds

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public Sound[] twoDSounds;
+        private static AudioManager _instance;
+        public AudioMixer mainAudioMixer;
+
+        public static AudioManager Instance => _instance;
+
+        private void Awake () {
+
+            if (_instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+        
+            foreach (Sound s in twoDSounds)
+            {
+                s.source = gameObject.AddComponent<AudioSource>();
+                s.source.clip = s.clip;
+                s.source.playOnAwake = false;
+                s.source.volume = s.volume;
+                s.source.pitch = s.pitch;
+
+                if (s.audioMixerGroup)
+                {
+                    s.source.outputAudioMixerGroup = s.audioMixerGroup;
+                }
+
+                s.source.loop = s.loop;
+            }
+        }
+
+        public void Play(string name, bool oneShot = false) {
+            var s = Array.Find(twoDSounds, sound => sound.name == name);
+            if (s == null) {
+                Debug.LogWarning("Sound: " + name + "not found!");
+                return;
+            }
+            if (!oneShot)
+                s.source.Play();
+            else 
+                s.source.PlayOneShot(s.source.clip);
+        }
+
+        public void Stop(string name)
+        {
+            var s = Array.Find(twoDSounds, sound => sound.name == name);
+            if (s == null) {
+                Debug.LogWarning("Sound: " + name + "not found!");
+                return;
+            }
+            s.source.Stop();
+        }
+
+        public AudioSource GetAudioSource(string name)
+        {
+            return Array.Find(twoDSounds, sound => sound.name == name).source;
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/CharacterAudioController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class CharacterAudioController : MonoBehaviour
+    {
+        [SerializeField] private List<AudioClip> footstepSounds;
+        [SerializeField] private AudioSource baseAudioSource;
+        [SerializeField] private AudioSource footstepAudioSource;
+        
+
+        private void Awake()
+        {
+            CopyValuesFromBaseAudioSource();
+        }
+
+        public void CopyValuesFromBaseAudioSource()
+        {
+            if (!baseAudioSource) return;
+            // copy settings from base audio source
+            footstepAudioSource.volume = baseAudioSource.volume;
+            footstepAudioSource.pitch = baseAudioSource.pitch;
+            footstepAudioSource.loop = baseAudioSource.loop;
+            if (baseAudioSource.outputAudioMixerGroup)
+            {
+                footstepAudioSource.outputAudioMixerGroup = baseAudioSource.outputAudioMixerGroup;
+            }
+            footstepAudioSource.playOnAwake = baseAudioSource.playOnAwake;
+            footstepAudioSource.spatialBlend = baseAudioSource.spatialBlend;
+            footstepAudioSource.dopplerLevel = baseAudioSource.dopplerLevel;
+            footstepAudioSource.minDistance = baseAudioSource.minDistance;
+            footstepAudioSource.maxDistance = baseAudioSource.maxDistance;
+            footstepAudioSource.rolloffMode = baseAudioSource.rolloffMode;
+            footstepAudioSource.bypassEffects = baseAudioSource.bypassEffects;
+            footstepAudioSource.SetCustomCurve(AudioSourceCurveType.CustomRolloff, baseAudioSource.GetCustomCurve(AudioSourceCurveType.CustomRolloff));
+            footstepAudioSource.SetCustomCurve(AudioSourceCurveType.ReverbZoneMix, baseAudioSource.GetCustomCurve(AudioSourceCurveType.ReverbZoneMix));
+            footstepAudioSource.SetCustomCurve(AudioSourceCurveType.SpatialBlend, baseAudioSource.GetCustomCurve(AudioSourceCurveType.SpatialBlend));
+            footstepAudioSource.SetCustomCurve(AudioSourceCurveType.Spread, baseAudioSource.GetCustomCurve(AudioSourceCurveType.Spread));
+        }
+        
+        public void PlayFootstepSound()
+        {
+            if (footstepSounds.Count == 0) return;
+            
+            var randomIndex = Random.Range(0, footstepSounds.Count);
+            footstepAudioSource.PlayOneShot(footstepSounds[randomIndex]);
+        }
+
+    }
+    
+#if UNITY_EDITOR
+
+    [CustomEditor(typeof(CharacterAudioController))]
+    public class CharacterAudioControllerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+        
+            var controller = (CharacterAudioController)target;
+            if (GUILayout.Button("Play Footstep Sound"))
+            {
+                controller.PlayFootstepSound();
+            }
+            if (GUILayout.Button("Copy Settings from Base Audio Source"))
+            {
+                controller.CopyValuesFromBaseAudioSource();
+            }
+        }
+    }
+#endif
+}
+
